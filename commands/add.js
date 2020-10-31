f3961feb364966e66ad2c42bc222718bbe9a55e6
Ait Zeouay Amrane <a.zeouayamrane@gmail.com>
2020-10-31 14:11:21 +0100

    [Evo] Wait until task is finished

--- commands/add.js
+++ commands/add.js
@@ -4,6 +4,27 @@ const ora = require("ora");
 const initialize = require("./init");
 const { isUrl } = require("../lib/validation");
 
+const MAX_RETRIES = 5;
+
+let retries = 0;
+
+const waitForTaskToFinish = (synology, id) => {
+  synology.tasksInfo().then((task) => {
+    console.log(`Task ${JSON.stringify(task)}`);
+    if (task.status === "finished") {
+      console.log(`Task ${task.id} finished`);
+    } else {
+      if (retries === MAX_RETRIES) {
+        console.error(`Timeout for getting info of the task ${task.id}`);
+        process.exit(1);
+      }
+      retries++;
+      console.log(`Task ${task.id} status is ${task.status}`);
+      setTimeout(() => waitForTaskToFinish(synology, id), 3000);
+    }
+  });
+};
+
 const create = async () => {
   // initialize synology connection
   const synology = await initialize();
@@ -26,7 +47,11 @@ const create = async () => {
   spinner.stop();
 
   if (task.success === true) {
-    console.log(chalk.green("Download task successfully created"));
+    console.log(
+      chalk.green("Download task successfully created"),
+      JSON.stringify(task)
+    );
+    waitForTaskToFinish(synology, task.id);
   } else {
     console.log(
       chalk.white.bgRed("There was a problem while trying to create the task")
