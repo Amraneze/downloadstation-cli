31e681d2907abbfbc5420f51b49bdef8da1cb8c8
Ait Zeouay Amrane <a.zeouayamrane@gmail.com>
2020-10-31 13:53:12 +0100

    [Evo] Use url as argument instead of input

--- commands/add.js
+++ commands/add.js
@@ -1,36 +1,35 @@
-const inquirer = require('inquirer');
-const chalk = require('chalk');
-const ora = require('ora');
+const chalk = require("chalk");
+const ora = require("ora");
 
-const initialize = require('./init');
-const { isUrl } = require('../lib/validation');
+const initialize = require("./init");
+const { isUrl } = require("../lib/validation");
 
 const create = async () => {
   // initialize synology connection
   const synology = await initialize();
 
   // get download url
-  const answers = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'url',
-      message: 'Download URL',
-      validate: isUrl,
-    },
-  ]);
+  const [, , , url] = process.argv;
 
-  const spinner = ora('Adding new task ...').start();
+  if (!isUrl(url)) {
+    console.error(`The url ${url} provided is not valid`);
+    process.exit(1);
+  }
+
+  console.info(`The provided url is ${url}`);
+
+  const spinner = ora("Adding new task ...").start();
 
   // create new task
-  const task = await synology.create(answers.url);
+  const task = await synology.create(url);
 
   spinner.stop();
 
   if (task.success === true) {
-    console.log(chalk.green('Download task successfully created'));
+    console.log(chalk.green("Download task successfully created"));
   } else {
     console.log(
-      chalk.white.bgRed('There was a problem while trying to create the task')
+      chalk.white.bgRed("There was a problem while trying to create the task")
     );
   }
 };
